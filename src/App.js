fee3cf8ca37fdbc231fb00fd0aa150afce6b5499
matine-gh <matine.ghazanfari@gmail.com>
2023-08-13 10:53:31 +0330

    init project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useTranslation} from "react-i18next";
+import './i18n';
+import {useState} from "react";
 
 function App() {
+
+  const { t, i18n } = useTranslation();
+  const [language, setLanguage] = useState('en');
+
+  const changeLanguageHandler = (lang) =>
+  {
+    setLanguage(lang)
+    i18n.changeLanguage(`${lang}`)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header bg-cyan-950 text-white text-2xl flex justify-center items-center flex-col min-h-screen">
+
+        <form className="my-16">
+          <label>{t("choose your language")}</label>
+          <select value={language}
+                  onChange={(e)=>changeLanguageHandler(e.target.value)}
+                  className="bg-inherit border-2 mx-4">
+            <option value="en">{t('En')}</option>
+            <option value="fr">{t('Fr')}</option>
+            <option value="rus">{t('Rus')}</option>
+          </select>
+        </form>
+
+        <div className="w-fit h-96 p-8 border-2 flex flex-col justify-around items-stretch">
+          <h1 className="text-3xl text-center">{t('menu')}</h1>
+          <div className="text-amber-600">
+            <li>{t('pizza')}: <span>{t('currency')+t('8')}</span></li>
+            <li>{t('pasta')}: <span>{t('currency')+t('5')}</span></li>
+            <li>{t('kebab')}: <span>{t('currency')+t('10')}</span></li>
+            <li>{t('Soup')}: <span>{t('currency')+t('3')}</span></li>
+          </div>
+        </div>
       </header>
     </div>
   );
---
+++ src/texts.jsx
@@ -0,0 +1,46 @@
+const resources = {
+  en: {
+    translation: {
+      "En": "English",
+      "Fr": "French",
+      "Rus": "Russian",
+      "currency": "$",
+      "choose your language": "choose your language",
+      "menu": "menu",
+      "pizza": "pizza",
+      "pasta": "pasta",
+      "kebab": "kebab",
+      "soup": "soup",
+    }
+  },
+  fr: {
+    translation: {
+      "En": "anglaise",
+      "Fr": "Français",
+      "Rus": "russe",
+      "currency": "€",
+      "choose your language": "Choisissez votre langue",
+      "menu": "menu",
+      "pizza": "pizza",
+      "pasta": "Pâtes",
+      "kebab": "kebab",
+      "soup": "Soupe",
+    }
+  },
+  rus: {
+    translation: {
+      "En": "английский язык",
+      "Fr": "Французский",
+      "Rus": "Русский",
+      "currency": "₽",
+      "choose your language": "Выберите ваш язык",
+      "menu": "Меню",
+      "pizza": "Пицца",
+      "pasta": "Макаронные изделия",
+      "kebab": "Барбекю",
+      "soup": "Суп",
+    }
+  }
+};
+
+export default resources
